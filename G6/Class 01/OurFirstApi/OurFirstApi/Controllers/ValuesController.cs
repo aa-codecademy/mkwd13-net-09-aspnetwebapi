6a59474b37991b29628889a30c93dc2a4c5ddfb6
tijanastojanovska <stojanovska_tijana@outlook.com>
2025-08-05 20:27:38 +0200

    Class 01

---
+++ G6/Class 01/OurFirstApi/OurFirstApi/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		//http://localhost:[port]/api/values
+		[HttpGet] //no additional info
+		public IEnumerable<string> GetStrings()
+		{
+			return new List<string> { "value1", "value2" };
+		}
+
+		//http://localhost:[port]/api/values/info
+		[HttpGet("info")]
+		public string GetInfo()
+		{
+			return "This is our values controller";
+		}
+	}
+}
