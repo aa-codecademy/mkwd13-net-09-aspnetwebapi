59014e8df268dda68e919228c1c57bd66d2d4dda
todorpelivanov <tpelivanov@gmail.com>
2025-08-05 21:05:53 +0200

    Class01

---
+++ G2/Class01/OurFirstWebApi/OurFirstWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet] //https://localhost:44319/api/values
+        public IEnumerable<string> GetStrings() 
+        {
+            return new[] { "value1", "value2" };
+        }
+    }
+}
