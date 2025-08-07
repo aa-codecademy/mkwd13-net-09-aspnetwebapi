4f16b60c86834fc09fff6bded8fef56810a24af7
tijanastojanovska <stojanovska_tijana@outlook.com>
2025-08-07 20:41:53 +0200

    Class 02

---
+++ G6/Class 02/NotesApp/NotesApp/Controllers/NotesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotesApp.Controllers
+{
+	[Route("api/[controller]")] //http://localhost:[port]/api/notes
+	[ApiController]
+	public class NotesController : ControllerBase
+	{
+		[HttpGet] //http://localhost:[port]/api/notes
+		public ActionResult<List<string>> Get()
+		{
+			//return StatusCode(StatusCodes.Status200OK, StaticDb.SimpleNotes);
+			return Ok(StaticDb.SimpleNotes);
+		}
+
+		[HttpGet("{index}")] //http://localhost:[port]/api/notes/1
+		public ActionResult<string> GetByIndex(int index)
+		{
+			try
+			{
+				//validations
+				if(index < 0)
+				{
+					//return StatusCode(StatusCodes.Status400BadRequest, "The index has negative value");
+					return BadRequest("The index has negative value");
+				}
+
+				if(index >= StaticDb.SimpleNotes.Count)
+				{
+					return StatusCode(StatusCodes.Status404NotFound, $"There is no resource on index {index}");
+				}
+
+				//throw new Exception("Test exception for status 500");
+				return Ok(StaticDb.SimpleNotes[index]);
+
+			}catch(Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. Please contact your administrator.");
+			}
+		}
+
+		[HttpGet("{noteId}/{userId}")] //http://localhost:[port]/api/notes/1/2
+		public ActionResult<string> GetNoteByIdAndUserId(int noteId, int userId)
+		{
+			try
+			{
+				if (noteId < 0 || userId < 0)
+				{
+					return BadRequest("The ids cannot have negative values");
+				}
+				return Ok($"Returning note with id {noteId} for user with id {userId}");
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+			}
+		}
+	}
+}
---
+++ G6/Class 02/NotesApp/NotesApp/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotesApp.Controllers
+{
+	[Route("api/[controller]")] //http://localhost:[port]/api/values
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		[HttpGet] //no additional info
+				  //http://localhost:[port]/api/values
+		public List<string> Get()
+		{
+			return new List<string>() { "value1", "value2" };
+		}
+
+		//[HttpGet] //ERROR - has same http method and same address
+		//		  //http://localhost:[port]/api/values
+		//public int GetNumber()
+		//{
+		//	return 1;
+		//}
+
+		[HttpGet("number")] //http://localhost:[port]/api/values/number
+		public int GetNumber()
+		{
+			return 1;
+		}
+
+		[HttpPost] //This is okay, beacuse it uses a different http method, although the route seems the same 
+		//http://localhost:[port]/api/values
+		public string GetString()
+		{
+			return "Hello";
+		}
+
+		[HttpGet("details/{id}")] //http://localhost:[port]/api/values/details/1
+		public int GetId(int id)
+		{
+			return id;
+		}
+
+		[HttpGet("{id}")] //http://localhost:[port]/api/values/1
+		public int GetFirstNumber(int id)
+		{
+			return id;
+		}
+
+		[HttpGet("{userId}/book/{bookId}")] //http://localhost:[port]/api/values/1/book/3
+		public string GetUserAndBook(int userId, int bookId)
+		{
+			return $"User: {userId}, book: {bookId}";
+		}
+
+		[HttpGet("{number}/movies")] //http://localhost:[port]/api/values/2/movies
+		public List<string> GetMovies(int number)
+		{
+			return new List<string>() { "Deadpool", "Joker" };
+		}
+	}
+}
