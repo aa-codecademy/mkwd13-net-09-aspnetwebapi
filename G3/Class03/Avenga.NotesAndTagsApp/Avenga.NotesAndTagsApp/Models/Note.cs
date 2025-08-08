577bacaee29b111b013290620053e5bb3ce8bca4
Danilo Borozan <daniloborozan07@gmail.com>
2025-08-08 20:51:48 +0200

    add code

---
+++ G3/Class03/Avenga.NotesAndTagsApp/Avenga.NotesAndTagsApp/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿using Avenga.NotesAndTagsApp.Models.Enums;
+
+namespace Avenga.NotesAndTagsApp.Models
+{
+    public class Note
+    {
+        public string Text { get; set; }
+        public Priority Priority { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+}
