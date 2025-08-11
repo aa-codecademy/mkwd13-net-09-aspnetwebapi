efeb793d8a1b336cfa8445fad8817a640df537be
Danilo Borozan <daniloborozan07@gmail.com>
2025-08-11 19:30:57 +0200

    add

---
+++ G3/Class04/Avenga.NotesAndTagsApp/Avenga.NotesAndTagsApp/Controllers/NotesController.cs
@@ -0,0 +1,102 @@
+﻿using Avenga.NotesAndTagsApp.DTOs;
+using Avenga.NotesAndTagsApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avenga.NotesAndTagsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<NoteDto>> GetAllNotes()
+        {
+            try
+            {
+                //db
+                var notesDb = StaticDb.Notes;
+                //map
+                var notes = notesDb.Select(x=> new NoteDto
+                {
+                    Text = x.Text,
+                    Priority = x.Priority,
+                    User = $"{x.User.FirstName} {x.User.LastName}",
+                    Tags = x.Tags.Select(t=>t.Name).ToList()
+                }).ToList();
+
+                //response
+                return Ok(notes);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<NoteDto> GetNoteById(int id)
+        {
+            try
+            {
+                if(id < 0)
+                {
+                    return BadRequest("The id can not be negative!");
+                }
+
+                Note noteDb = StaticDb.Notes.SingleOrDefault(x=> x.Id == id);
+                if (noteDb == null)
+                { 
+                    return NotFound($"Note with id {id} does not exist!");
+                }
+
+                NoteDto noteDto = new NoteDto()
+                {
+                    Text = noteDb.Text,
+                    Priority = noteDb.Priority,
+                    User = $"{noteDb.User.FirstName} {noteDb.User.LastName}",
+                    Tags = noteDb.Tags.Select(t=>t.Name).ToList()
+                };
+
+                return Ok(noteDto);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("findById")]
+        public ActionResult<NoteDto> FindById(int id)
+        {
+            try
+            {
+                if (id < 0)
+                {
+                    return BadRequest("The id can not be negative!");
+                }
+
+                Note noteDb = StaticDb.Notes.SingleOrDefault(x => x.Id == id);
+                if (noteDb == null)
+                {
+                    return NotFound($"Note with id {id} does not exist!");
+                }
+
+                NoteDto noteDto = new NoteDto()
+                {
+                    Text = noteDb.Text,
+                    Priority = noteDb.Priority,
+                    User = $"{noteDb.User.FirstName} {noteDb.User.LastName}",
+                    Tags = noteDb.Tags.Select(t => t.Name).ToList()
+                };
+
+                return Ok(noteDto);
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ G3/Class04/Avenga.NotesAndTagsApp/Avenga.NotesAndTagsApp/DTOs/NoteDto.cs
@@ -0,0 +1,12 @@
+﻿using Avenga.NotesAndTagsApp.Models.Enums;
+
+namespace Avenga.NotesAndTagsApp.DTOs
+{
+    public class NoteDto
+    {
+        public string Text { get; set; }
+        public PriorityEnum Priority { get; set; }
+        public string User {  get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
