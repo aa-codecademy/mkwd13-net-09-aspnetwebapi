f5ecfab5fbeca3e4d5ff5c8aa9a5eef8f69d193e
tijanastojanovska <stojanovska_tijana@outlook.com>
2025-08-15 20:45:56 +0200

    Class 05

---
+++ G6/Class 05/MoviesApp/MoviesApp/Controllers/MoviesController.cs
@@ -0,0 +1,195 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoviesApp.Models;
+using MoviesApp.Models.DTOs;
+using MoviesApp.Models.Enums;
+
+namespace MoviesApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MoviesController : ControllerBase //http://localhost:[port]/api/Movies
+	{
+		[HttpGet] //http://localhost:[port]/api/Movies
+		public ActionResult<List<MovieDto>> GetAll()
+		{
+			try
+			{
+				var moviesDb = StaticDb.Movies;
+				if(moviesDb == null)
+				{
+					return Ok(new List<MovieDto>()); //we haven't added any movies yet  - not an error, just an empty collection
+				}
+
+				var moviesDto = moviesDb.Select(x => new MovieDto //we need to map the data from the db (Movie domain class) into MovieDto
+				{
+					Title = x.Title,
+					Description	= x.Description,
+					Year = x.Year,
+					Genre = x.Genre
+				});
+
+				return Ok(moviesDto);
+			}catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+			}
+		}
+
+		//we must send the path param because it is a part of the basic route - it differences this route from the other routes
+		//if we don't send the path param - then this endpoint cannot be hit (it will go to the route //http://localhost:[port]/api/Movies (GetAll))
+		[HttpGet("{id}")] //http://localhost:[port]/api/Movies/1
+		public ActionResult<MovieDto> GetByPathId(int id)
+		{
+			try
+			{
+				//validations
+				if(id <= 0)
+				{
+					return BadRequest("Bad request, the id cannot be a negative number");
+				}
+
+				var movieDb = StaticDb.Movies.FirstOrDefault(x => x.Id == id);
+				if(movieDb == null)
+				{
+					return NotFound($"Movie with id {id} was not found");
+				}
+
+				//map
+				var movieDto = new MovieDto
+				{
+					Description = movieDb.Description,
+					Year = movieDb.Year,
+					Genre = movieDb.Genre,
+					Title = movieDb.Title
+				};
+
+				return Ok(movieDto);
+
+			}catch(Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+			}
+		}
+
+		//it is totally valid to not send a query param - because a query param is not a part of the basic route, but it is added to the basic route - we can still hit this endpoint without the query param
+		[HttpGet("queryString")] //http://localhost:[port]/api/Movies/queryString - this is the route without the query string part - this is the basic route/url for this method
+								 //http://localhost:[port]/api/Movies/queryString?id=1 -we can add to the basic route for this method a query string where we can send the params
+		public ActionResult<MovieDto> GetByQueryStringId(int? id)
+		{
+			try
+			{
+				//validations
+				if(id == null)
+				{
+					return BadRequest("The id cannot be null");
+				}
+				if (id <= 0)
+				{
+					return BadRequest("Bad request, the id cannot be a negative number");
+				}
+
+				var movieDb = StaticDb.Movies.FirstOrDefault(x => x.Id == id);
+				if (movieDb == null)
+				{
+					return NotFound($"Movie with id {id} was not found");
+				}
+
+				//map
+				var movieDto = new MovieDto
+				{
+					Description = movieDb.Description,
+					Year = movieDb.Year,
+					Genre = movieDb.Genre,
+					Title = movieDb.Title
+				};
+
+				return Ok(movieDto);
+
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+			}
+		}
+
+		[HttpGet("filter")]  //http://localhost:[port]/api/movies/filter
+							 //http://localhost:[port]/api/movies/filter?genre=1
+							 //http://localhost:[port]/api/movies/filter?year=2022
+							 //http://localhost:[port]/api/movies/filter?year=2022&genre=1
+							 //http://localhost:[port]/api/movies/filter?genre=1&&year=2022
+		public ActionResult<List<MovieDto>> FilterMovies(int? genre, int? year)
+		{
+			try
+			{
+				if(genre == null && year == null) //the user  is allowed to skip the query string params and the route would still be valid, however from business logic aspect we need at least one param so that we can filter
+				{
+					return BadRequest("You have to send at least one filter param");
+				}
+
+				//check if the genre is in the correct range for the genre enum - only when genre has value (a value for genre was sent)
+				if (genre.HasValue) //HasValue checks if genre is not null (has value)
+				{
+					var enumValues = Enum.GetValues(typeof(GenreEnum)) //returns  array of the values as type Enum
+									 .Cast<GenreEnum>()//we need our specific type of enum - GenreEnum, not the base Enum type, so we need to cast the Enum array into GenreEnum -> Comedy=1, Action=2....
+									 .Select(genre => (int)genre) //1,2,3...
+									 .ToList();
+
+					//genre is nullable, we need .Value to access its value if it was sent
+					if (!enumValues.Contains(genre.Value)) //ex.we sent 15 as genre
+					{
+						return NotFound($"The genre with id {genre.Value} was not found");
+					}
+									 } 
+
+				if(year == null) //here we can be sure that the genre has value, because we already checked the scenario where both of them dont have values. So, if year does not have value - then genre must have a value
+				{
+					List<Movie> moviesDbByGenre = StaticDb.Movies.Where(x => (int)x.Genre == genre.Value).ToList();
+					//List<Movie> moviesDbByGenre = StaticDb.Movies.Where(x => x.Genre == (GenreEnum)genre.Value).ToList();
+
+					var moviesGenreDto = moviesDbByGenre.Select(x => new MovieDto
+					{
+						Description = x.Description,
+						Genre = x.Genre,
+						Year = x.Year,
+						Title = x.Title
+					});
+
+					return Ok(moviesGenreDto);
+				}
+
+				if(genre == null)//here we can be sure that the year has value, because we already checked the scenario where both of them dont have values. So, if genre does not have value - then year must have a value
+				{
+					var moviesDbByYear = StaticDb.Movies.Where(x => x.Year == year).ToList();
+
+					var moviesYearDto = moviesDbByYear.Select(x => new MovieDto
+					{
+						Description = x.Description,
+						Genre = x.Genre,
+						Year = x.Year,
+						Title = x.Title
+					});
+
+					return Ok(moviesYearDto);
+				}
+
+				List<Movie> moviesDb = StaticDb.Movies.Where(x => (int)x.Genre == genre.Value && x.Year == year).ToList();
+				var moviesDto = moviesDb.Select(x => new MovieDto
+				{
+					Description = x.Description,
+					Genre = x.Genre,
+					Year = x.Year,
+					Title = x.Title
+				});
+
+				return Ok(moviesDto);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+			}
+		}
+
+	}
+}
---
+++ G6/Class 05/MoviesApp/MoviesApp/Models/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace MoviesApp.Models
+{
+	public abstract class BaseEntity
+	{
+		public int Id { get; set; }
+	}
+}
