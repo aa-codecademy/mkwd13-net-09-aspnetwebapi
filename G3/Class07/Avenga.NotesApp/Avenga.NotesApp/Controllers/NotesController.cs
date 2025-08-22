c838ae190161441ff9f0c4ad1ea31947fea3fdeb
Danilo Borozan <daniloborozan07@gmail.com>
2025-08-22 19:09:40 +0200

    add code

--- G3/Class07/Avenga.NotesApp/Avenga.NotesApp/Controllers/NotesController.cs
+++ G3/Class07/Avenga.NotesApp/Avenga.NotesApp/Controllers/NotesController.cs
@@ -46,5 +46,63 @@ namespace Avenga.NotesApp.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); //staus code => 500
             }
         }
+
+        [HttpPost]
+        public IActionResult AddNote([FromBody] AddNoteDto addNoteDto) 
+        {
+            try
+            {
+                _noteService.AddNote(addNoteDto);
+                return StatusCode(StatusCodes.Status201Created, "Note added!"); //staus code => 201
+            }
+            catch(NoteDataException e)
+            {
+                return BadRequest(e.Message); //staus code => 400
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); //staus code => 500
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateNote([FromBody] UpdateNoteDto updateNoteDto)
+        {
+            try
+            {
+                _noteService.UpdateNote(updateNoteDto);
+                return NoContent(); // staus code => 204
+            }
+            catch (NoteNotFoundException e)
+            {
+                return NotFound(e.Message);//staus code => 404
+            }
+            catch(NoteDataException e)
+            {
+                return BadRequest(e.Message); //staus code => 400
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); //staus code => 500
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNote(int id)
+        {
+            try
+            {
+                _noteService.DeleteNote(id);
+                return Ok($"Note with id {id} successfully deleted!");
+            }
+            catch(NoteNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); //staus code => 500
+            }
+        }
     }
 }
