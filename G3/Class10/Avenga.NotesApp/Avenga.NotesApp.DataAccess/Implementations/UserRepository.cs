037e7127b0a8149dc1a7d969d88c2ea353c53283
Danilo Borozan <daniloborozan07@gmail.com>
2025-08-27 17:19:25 +0200

    add

---
+++ G3/Class10/Avenga.NotesApp/Avenga.NotesApp.DataAccess/Implementations/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using Avenga.NotesApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avenga.NotesApp.DataAccess.Implementations
+{
+    public class UserRepository : IRepository<User>
+    {
+        public NotesAppDbContext _noteAppDbContext;
+        public UserRepository(NotesAppDbContext noteAppDbContext)
+        {
+            _noteAppDbContext = noteAppDbContext; //DI
+        }
+        public void Add(User entity)
+        {
+            _noteAppDbContext.Users.Add(entity);
+            _noteAppDbContext.SaveChanges();
+        }
+
+        public void Delete(User entity)
+        {
+            _noteAppDbContext.Users.Remove(entity);
+            _noteAppDbContext.SaveChanges();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            return _noteAppDbContext.Users.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ G3/Class10/Avenga.NotesApp/Avenga.NotesApp.Helpers/DependencyInjectionHelper.cs
@@ -0,0 +1,35 @@
+﻿using Avenga.NotesApp.DataAccess;
+using Avenga.NotesApp.DataAccess.Implementations;
+using Avenga.NotesApp.Domain.Models;
+using Avenga.NotesApp.Services.Implementations;
+using Avenga.NotesApp.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avenga.NotesApp.Helpers
+{
+    public static class DependencyInjectionHelper
+    {
+        public static void InjectDbContext(IServiceCollection service)
+        {
+            service.AddDbContext<NotesAppDbContext>(x => x.UseSqlServer("Server=DANILO\\SQLEXPRESS;Database=NotesAppNew;Trusted_Connection=True;TrustServerCertificate=True;"));
+        }
+
+        public static void InjectRepositories(IServiceCollection service)
+        {
+            service.AddTransient<IRepository<Note>, NoteRepository>();
+            service.AddTransient<IRepository<User>, UserRepository>();
+        }
+
+        public static void InjectServices(IServiceCollection services) 
+        {
+            services.AddTransient<INoteService, NoteService>();
+        }
+
+    }
+}
