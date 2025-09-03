4c2f4b713653b7f8342bab99bb3cc54e6ecee9b8
Danilo Borozan <daniloborozan07@gmail.com>
2025-09-03 19:33:14 +0200

    add code

---
+++ G3/Class12/Unit_Testing/Service/RandomService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class RandomService
+    {
+        public int? Sum(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            if (num1 > 0 && num2 > 0 && sum < 0) return null;
+            return sum;
+        }
+
+        public bool isFirstNumberLarger(int num1, int num2) 
+        {
+            return num1 > num2;
+        }
+
+        public string GetDigitName(int num)
+        {
+            List<string> digitNames = new List<string>()
+            {
+                "Zero", "One", "Two", "Three", "Fourt"
+            };
+            return digitNames[num];
+        }
+    }
+}
---
+++ G3/Class12/Unit_Testing/Unit_Testing_Tests/RandomServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using Service;
+
+namespace Unit_Testing_Tests
+{
+    [TestClass]
+    public class RandomServiceTest
+    {
+        private readonly RandomService _randomService;
+
+        public RandomServiceTest(RandomService randomService)
+        {
+            _randomService = randomService;
+        }
+
+        [TestMethod]
+        public void Sum_BothNumbersAreValid_PositiveResultNumber()
+        {
+            //Arrange
+            int num1 = 10;
+            int num2 = 20;
+            int? expectedResult = 30;
+
+            //Act
+            int? result = _randomService.Sum(num1, num2);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Sum_largeNumberIntefers_Null()
+        {
+            //Arrange
+            int num1 = 2111111111;
+            int num2 = 2111111111;
+
+            //Act
+            int? result = _randomService.Sum(num1, num2);
+
+            //Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void isFirstNumberLarger_FirstNumberISLarger_True()
+        {
+            //Arrange
+            int num1 = 20;
+            int num2 = 12;
+
+            //Act
+            bool result = _randomService.isFirstNumberLarger(num1, num2);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetDigitName_FiveDigit_Exception()
+        {
+            //Arrange
+            int num = 5;
+
+            //Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=>_randomService.GetDigitName(num));
+        }
+
+    }
+}
