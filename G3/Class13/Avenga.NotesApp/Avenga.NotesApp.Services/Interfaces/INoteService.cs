28de296d9231f6086313a5d8510b2405f365b120
todorpelivanov <tpelivanov@gmail.com>
2025-09-05 20:34:56 +0200

    class13

---
+++ G3/Class13/Avenga.NotesApp/Avenga.NotesApp.Services/Interfaces/INoteService.cs
@@ -0,0 +1,18 @@
+﻿using Avenga.NotesApp.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avenga.NotesApp.Services.Interfaces
+{
+    public interface INoteService
+    {
+        List<NoteDto> GetAllNotes();
+        NoteDto GetByIdNote(int id);
+        void AddNote(AddNoteDto addNoteDto);
+        void UpdateNote(UpdateNoteDto updateNoteDto);
+        void DeleteNote(int id);
+    }
+}
---
+++ G3/Class13/Avenga.NotesApp/Avenga.NotesApp/Controllers/NotesController.cs
@@ -0,0 +1,114 @@
+﻿using Avenga.NotesApp.Dto;
+using Avenga.NotesApp.Services.Interfaces;
+using Avenga.NotesApp.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace Avenga.NotesApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+        public NotesController(INoteService noteService) //DI
+        {
+            _noteService = noteService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<NoteDto>> GetAllNotes()
+        {
+            try
+            {
+                return Ok(_noteService.GetAllNotes());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<NoteDto> GetNoteById(int id) 
+        {
+            try
+            {
+                NoteDto noteDto = _noteService.GetByIdNote(id);
+                Log.Information($"Retrieved note: {noteDto.Text}");
+                return Ok(noteDto); //satus code => 200
+            }
+            catch(NoteNotFoundException e)
+            {
+                Log.Warning($"The requested note was not found! {e.Message}");
+                return NotFound(e.Message); // staus code => 404
+            }
+            catch (Exception ex) 
+            {
+                Log.Error($"Internal Exception: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); //staus code => 500
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddNote([FromBody] AddNoteDto addNoteDto) 
+        {
+            try
+            {
+                _noteService.AddNote(addNoteDto);
+                Log.Information($"New note was added!");
+                return StatusCode(StatusCodes.Status201Created, "Note added!"); //staus code => 201
+            }
+            catch(NoteDataException e)
+            {
+                return BadRequest(e.Message); //staus code => 400
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); //staus code => 500
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateNote([FromBody] UpdateNoteDto updateNoteDto)
+        {
+            try
+            {
+                _noteService.UpdateNote(updateNoteDto);
+                return NoContent(); // staus code => 204
+            }
+            catch (NoteNotFoundException e)
+            {
+                return NotFound(e.Message);//staus code => 404
+            }
+            catch(NoteDataException e)
+            {
+                return BadRequest(e.Message); //staus code => 400
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); //staus code => 500
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNote(int id)
+        {
+            try
+            {
+                _noteService.DeleteNote(id);
+                return Ok($"Note with id {id} successfully deleted!");
+            }
+            catch(NoteNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message); //staus code => 500
+            }
+        }
+    }
+}
