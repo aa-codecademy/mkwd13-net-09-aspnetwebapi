5a5d9dc9b8936157f1b092e15df3dc396a7d200b
Danilo Borozan <daniloborozan07@gmail.com>
2025-09-16 17:48:13 +0200

    add code

---
+++ G3/Class15/Avenga.MovieApp/Avenga.MovieApp/Controllers/MoviesController.cs
@@ -0,0 +1,150 @@
+﻿using Avenga.MovieApp.DataAccess;
+using Avenga.MovieApp.Domain.Enums;
+using Avenga.MovieApp.Dtos;
+using Avenga.MovieApp.Services.Interfaces;
+using Avenga.MovieApp.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Avenga.MovieApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<MovieDto>> GetAllMovie()
+        {
+            try
+            {
+                int userId = GetAuthorizedUserId();
+                return Ok(_movieService.GetAllMovies(userId));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<MovieDto> GetMovieById(int id)
+        {
+            try
+            {
+                return Ok(_movieService.GetMovieById(id));
+            }
+            catch(MovieNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch(MovieException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie([FromBody] AddMovieDto addMovieDto)
+        {
+            try
+            {
+                int userId = GetAuthorizedUserId();
+                _movieService.AddMovie(addMovieDto, userId);
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (MovieException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMovie([FromBody] UpdateMovieDto updateMovieDto) 
+        {
+            try
+            {
+                _movieService.UpdateMovie(updateMovieDto);
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+            catch (MovieNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (MovieException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMovie(int id)
+        {
+            try
+            {
+                _movieService.DeleteMovie(id);
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+            catch (MovieNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (MovieException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("filterMovie")]
+        public IActionResult FilterMovie(int? year, GenreEnum? genre) 
+        {
+            try
+            {
+                return Ok(_movieService.FilterMovies(year, genre));
+            }
+            catch (MovieException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        private int GetAuthorizedUserId()
+        {
+            if (!int.TryParse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value, out int userId))
+            {
+                throw new UserException(userId, User.FindFirst(ClaimTypes.Name)?.Value, "Name indentifire claims not exist!");
+            }
+            return userId;
+        }
+    }
+}
---
+++ G3/Class15/Avenga.MovieApp/Avenga.MovieApp/Controllers/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using Avenga.MovieApp.Dtos.UserDto;
+using Avenga.MovieApp.Services.Interfaces;
+using Avenga.MovieApp.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avenga.MovieApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult LoginUser([FromBody] LoginUserDto loginUserDto)
+        {
+            try
+            {
+                UserDto user = _userService.LoginUser(loginUserDto);
+                if(user == null)
+                {
+                    return NotFound("Username or Password is incorrect!");
+                }
+                return Ok(user);
+            }
+            catch(UserException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult RegisterUser([FromBody] RegisterUserDto registerUserDto) 
+        {
+            try
+            {
+                _userService.RegisterUser(registerUserDto);
+
+                return Ok(new ResponseDto() { Success = "Successfully registered user!"});
+            }
+            catch (UserException e)
+            {
+                return BadRequest(new ResponseDto() { Error = e.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
